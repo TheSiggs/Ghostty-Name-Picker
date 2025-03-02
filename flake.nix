4f5af53b120d5bf67d1912132a121090f0a9cc51
Sam Siggs <sam.siggs@gmail.com>
2025-03-02 21:24:10 +1300

    Deploy to Google Cloud

--- flake.nix
+++ flake.nix
@@ -9,28 +9,32 @@
       let
         pkgs = import nixpkgs { inherit system; };
         python = pkgs.python312;
+        poetry = pkgs.poetry;
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = [
             python
-            pkgs.poetry
+            poetry  
             pkgs.cmake
             pkgs.google-cloud-sdk
-
             pkgs.gcc
             pkgs.libffi
           ];
+
           shellHook = ''
             export LD_LIBRARY_PATH=${pkgs.gcc.cc.lib}/lib:$LD_LIBRARY_PATH
             export GOOGLE_APPLICATION_CREDENTIALS="$HOME/.config/gcloud/application_default_credentials.json"
             export GOOGLE_CLOUD_PROJECT="ghostty-picker"
             export FLASK_SETTINGS_FILENAME="settings.py"
 
-            echo "Activating Poetry environment..."
+            # Ensure .venv is created
             if [ ! -d ".venv" ]; then
+              echo "Creating virtual environment..."
               poetry install
             fi
+
+            echo "Activating .venv"
             source .venv/bin/activate
           '';
         };
